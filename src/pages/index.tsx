c20ce7138b9bdb169d04a6e96d68685dfc91e191
a-hsu <a.hsu873@gmail.com>
2023-04-11 10:22:26 -0400

    basic copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div>Aeon Tactics</div>
+			<div className="justify-content">
+				<h1 className="font-bold">Aeon Tactics</h1>
+				<p className="flex text-center">
+					Aeon Tactics is a web based autobattler game. Build your team,
+					dominate your opponents, claim victory!
+				</p>
+			</div>
 		</main>
 	);
 }
