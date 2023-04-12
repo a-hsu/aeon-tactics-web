db0d282a88d6a2292bc2fe0be9773a0a5f5c1490
a-hsu <a.hsu873@gmail.com>
2023-04-12 01:21:23 -0400

    removed components index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Community from "./components/Community";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import PlayNow from "./components/PlayNow";
+import Team from "./components/Team";
 
-import { Community, Footer, Hero, Navbar, PlayNow, Team } from "./components";
+// import { Community, Footer, Hero, Navbar, PlayNow, Team } from "./components";
 
 export default function Home() {
 	return (
