c682dbfb5286829667b80c67598616f71e584747
a-hsu <a.hsu873@gmail.com>
2023-04-18 05:46:11 -0400

    created sample news articles and editing news grid

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -2,6 +2,26 @@ import Link from "next/link";
 import BasicDivider from "./Dividers/BasicDivider";
 
 const News = () => {
+	const newsArticles = [
+		{
+			title: "Patch 0.1",
+			author: "Huck",
+			subtext: "",
+			body: "",
+		},
+		{
+			title: "Aeon List",
+			author: "Huck",
+			subtext: "",
+			body: "",
+		},
+		{
+			title: "How to Play Aeon Tactics",
+			author: "Huck",
+			subtext: "",
+			body: "",
+		},
+	];
 	return (
 		<>
 			<section
@@ -11,14 +31,14 @@ const News = () => {
 				{/* <BasicDivider /> */}
 				{/* Content */}
 				{/* The goal here is to be able to programmatically make this list adjust based on the top 3 most recent articles */}
-				<div className="flex flex-col mt-40 mx-24">
+				<div className="flex flex-col mt-40 mx-12">
 					{/* Grid */}
-					<div className="grid grid-cols-3 grid-rows-2 gap-4">
+					<div className="grid grid-cols-1 grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 gap-4">
 						{["01", "02", "03", "11", "12"].map(e => {
 							return (
 								<div
 									key={`${e}`}
-									className="border-2 border-solid border-black h-52 m-0 p-0 flex flex-row"
+									className="border-2 border-solid border-black h-52 m-0 lg:aspect-video flex flex-row"
 								>
 									<div className="m-0 p-0 h-full w-2/5 border-red-400 border-2 border-solid">
 										<img src="" alt="" />
@@ -29,10 +49,13 @@ const News = () => {
 						})}
 					</div>
 				</div>
-				<div className="flex items-center my-12 p-2 justify-center">
-					<button className="mx-auto">
-						<Link href="/news" className="text-center tracking-tighter underline-offset-4 underline hover:text-burnt-orange-500 transition duration-150">VIEW ALL</Link>
-					</button>
+				<div className="flex items-center my-12 p-0 justify-center">
+					<Link
+						href="/news"
+						className="tracking-tighter underline-offset-4 underline hover:text-burnt-orange-500 transition duration-150"
+					>
+						VIEW ALL
+					</Link>
 				</div>
 			</section>
 		</>
